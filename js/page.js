6983678bdce9a2aa06aca9c018f86b9dc89e6e8c
Melina Magali Quiroz <melumaguiquiroz96@hotmail.com>
2021-09-13 10:31:32 -0300

    nav js

--- js/page.js
+++ js/page.js
@@ -1,20 +1,15 @@
-$(document).ready(function() {
-
-    $(window).scroll(function() {
-        let scrollDist = $(document).scrollTop();
-        if(scrollDist > 200) {
-            $('.nav').addClass('sticky');
-            $(".logo").width("90px").heigth("90px");
-            $(".logo").animated({
-                left: 90,
-            })
-                 
-        }
-         else {
-            $('.nav').removeClass('sticky');
-            $(".logo").width("130px").heigth("130px");
-        }
-         
+$(document).ready(function () {
+ 
+    /*--Sticky Navigation---*/
+    $(window).scroll(function () {
+      let scrollDist = $(document).scrollTop();
+      if (scrollDist > 150) {
+        $(".nav").addClass("sticky");
+        $(".logo").width("90px").heigth("90px");
+      } else {
+        $(".nav").removeClass("sticky");
+        $(".logo").width("130px").heigth("130px").css("transition", "all 0.25s");
+      }
     });
-
-});
+  });
+  
