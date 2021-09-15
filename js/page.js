b6433596dc1c252713d0d3a47e97363486ae2afc
Melina Magali Quiroz <melumaguiquiroz96@hotmail.com>
2021-09-14 23:05:54 -0300

    nav menu

--- js/page.js
+++ js/page.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
- 
-    /*--Sticky Navigation---*/
-    $(window).scroll(function () {
-      let scrollDist = $(document).scrollTop();
-      if (scrollDist >= 150) {
-        $(".nav").addClass("sticky");
-        $(".logo").width("90px").heigth("90px");
-      } else {
-        $(".nav").removeClass("sticky");
-        $(".logo").width("130px").heigth("130px").css("transition", "all 0.25s");
-      }
-    });
+  /*--Sticky Navigation---*/
+  $(window).scroll(function () {
+    let header = $("header");
+    if ($(this).scrollTop() > 50) {
+      if (!header.data("faded"))
+        header.data("faded", 1).stop(true).fadeTo(400, 0.5);
+      $(".nav").addClass("sticky");
+      $(".logo").width("90px").heigth("90px");
+    } else if (header.data("faded")) {
+      header.data("faded", 0).stop(true).fadeTo(400, 1);
+      $(".nav").removeClass("sticky");
+      $(".logo").width("130px").heigth("130px");
+    }
   });
-  
+});
