e24c89d1c867b45039620afa4a62b9afb1db8bfc
Melina Magali Quiroz <melumaguiquiroz96@hotmail.com>
2021-09-22 17:46:27 -0300

    inicio

--- js/page.js
+++ js/page.js
@@ -1,16 +1,18 @@
-$(document).ready(function () {
-  /*--Sticky Navigation---*/
-  $(window).scroll(function () {
-    let header = $("header");
-    if ($(this).scrollTop() > 50) {
-      if (!header.data("faded"))
-        header.data("faded", 1).stop(true).fadeTo(400, 0.5);
-      $(".nav").addClass("sticky");
-      $(".logo").width("90px").heigth("90px");
-    } else if (header.data("faded")) {
-      header.data("faded", 0).stop(true).fadeTo(400, 1);
-      $(".nav").removeClass("sticky");
-      $(".logo").width("130px").heigth("130px");
-    }
-  });
-});
+// $(document).ready(function () {
+  
+//   /*--Sticky Navigation---*/
+//   $(window).scroll(function () {
+//     let header = $("header");
+//     if ($(this).scrollTop() > 50) {
+//       if (!header.data("faded"))
+//         header.data("faded", 1).stop(true).fadeTo(400, 0.5);
+//       $(".nav").addClass("sticky");
+//       $(".logo").width("90px").heigth("90px");
+//     } else if (header.data("faded")) {
+//       header.data("faded", 0).stop(true).fadeTo(400, 1);
+//       $(".nav").removeClass("sticky");
+//       $(".logo").width("130px").heigth("130px");
+//     }
+//   });
+// });
+
