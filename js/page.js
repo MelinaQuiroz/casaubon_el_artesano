6f004102e9bbb079e65fec6619d4dc0389bcb9fa
Melina Magali Quiroz <melumaguiquiroz96@hotmail.com>
2021-09-27 22:55:19 -0300

    inicio

--- js/page.js
+++ js/page.js
@@ -1,33 +1,29 @@
 $(document).ready(function () {
-/*---HAMBURGUER BTN---*/
-const menuBtn = $(".menu-btn");
-let menuOpen = false;
-menuBtn.click( () => {
+  /*---HAMBURGUER BTN---*/
+  const menuBtn = $(".menu-btn");
+  let menuOpen = false;
+  menuBtn.click(() => {
     if (!menuOpen) {
-        menuBtn.addClass("open");
-        menuOpen = true;
+      menuBtn.addClass("open");
+      menuOpen = true;
     } else {
-        menuBtn.removeClass("open");
-        menuOpen = false;
+      menuBtn.removeClass("open");
+      menuOpen = false;
     }
+  });
 
-})
-
-  
-  
-//   /*--Sticky Navigation---*/
-//   $(window).scroll(function () {
-//     let header = $("header");
-//     if ($(this).scrollTop() > 50) {
-//       if (!header.data("faded"))
-//         header.data("faded", 1).stop(true).fadeTo(400, 0.5);
-//       $(".nav").addClass("sticky");
-//       $(".logo").width("90px").heigth("90px");
-//     } else if (header.data("faded")) {
-//       header.data("faded", 0).stop(true).fadeTo(400, 1);
-//       $(".nav").removeClass("sticky");
-//       $(".logo").width("130px").heigth("130px");
-//     }
-//   });
+  //   /*--Sticky Navigation---*/
+  //   $(window).scroll(function () {
+  //     let header = $("header");
+  //     if ($(this).scrollTop() > 50) {
+  //       if (!header.data("faded"))
+  //         header.data("faded", 1).stop(true).fadeTo(400, 0.5);
+  //       $(".nav").addClass("sticky");
+  //       $(".logo").width("90px").heigth("90px");
+  //     } else if (header.data("faded")) {
+  //       header.data("faded", 0).stop(true).fadeTo(400, 1);
+  //       $(".nav").removeClass("sticky");
+  //       $(".logo").width("130px").heigth("130px");
+  //     }
+  //   });
 });
-
