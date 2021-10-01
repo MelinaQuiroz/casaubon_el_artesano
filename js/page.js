069e52248c4f8a6c3fb0ebaa4904c15f36c4e01a
Melina Magali Quiroz <melumaguiquiroz96@hotmail.com>
2021-09-30 21:36:53 -0300

    inicio

--- js/page.js
+++ js/page.js
@@ -12,18 +12,14 @@ $(document).ready(function () {
     }
   });
 
-  //   /*--Sticky Navigation---*/
-  //   $(window).scroll(function () {
-  //     let header = $("header");
-  //     if ($(this).scrollTop() > 50) {
-  //       if (!header.data("faded"))
-  //         header.data("faded", 1).stop(true).fadeTo(400, 0.5);
-  //       $(".nav").addClass("sticky");
-  //       $(".logo").width("90px").heigth("90px");
-  //     } else if (header.data("faded")) {
-  //       header.data("faded", 0).stop(true).fadeTo(400, 1);
-  //       $(".nav").removeClass("sticky");
-  //       $(".logo").width("130px").heigth("130px");
-  //     }
-  //   });
+    /*--Sticky Navigation---*/
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 100) {
+        $("nav").addClass("sticky");
+       
+      } else {
+        $("nav").removeClass("sticky");
+      
+      }
+    });
 });
