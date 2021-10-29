940edcc5675fad04107f4c935883f6a9a8d4da23
Lucia Bjorklund <luubjorklund@gmail.com>
2021-10-29 15:13:08 -0300

    Segundo Carrousel

--- js/page.js
+++ js/page.js
@@ -69,7 +69,7 @@ $(document).ready(function () {
       $(window).scroll(function(){
           if($(window).scrollTop() < 200) {
               $('#logo')
-                  .css({'width':'550px' , 'height':'auto'
+                  .css({'width':'450px' , 'height':'auto'
                
                 })
                 $("#logo-min").css({ "opacity":"0%" ,
